c7288e14e344de74a7c5687a85b0df7804325b51
Herman Tibor <therman89@gmail.com>
2017-07-25 23:16:21 +0200

    Changed to BLE

--- LogThunderboard/LogThunderboard.py
+++ LogThunderboard/LogThunderboard.py
@@ -5,86 +5,53 @@ import urllib2
 #import requests
 import re
 from datetime import datetime
+import pygatt
+import struct
 
-class MeasData():
-	ReadKey = '0'
-	ID = 0
-	humidity = 0.0
-	temp = 0.0
-	amblight = 0
-	UV = 0
-	RSSI = 0
-	Vbat = 0
-MEAS_DATA_LINES = 10
-NumberOfLines = 0
-nodecnt = 2
-Node = [MeasData() for i in  range(nodecnt)]
-ser = serial.Serial('/dev/ttyACM0',115200)
-#ser = serial.Serial('COM17',115200)
-ser.timeout = 1 
 Node[0].ReadKey = 'DW24J25W85FHJ6XD'
 Node[1].ReadKey = 'M6UVE1VBFQGNHNDI'
 
-nodeidx = 0
-while(True):
-	line = ser.readline()
-	line = line.strip()
-	try:
-		if(line != ''):
-			#print line
-			NumberOfLines = NumberOfLines + 1
-			words = line.split(':')
-			#print words[0].strip()
-			mName = words[0]
-			words[1] = words[1].strip()
-			try:
-				mData = int(words[1])
-			except ValueError:
-				mData = float(words[1])
-			if("Node" in mName):
-				#print "Node data type " + str(type(mData))
-				if (mData > 0 and mData <= nodecnt):
-					nodeidx = mData-1
-					#print "Node " + str(mData)
-			elif("Humidity" in mName):
-				Node[nodeidx].humidity = mData
-				#print "Humidity " + str(mData)
-			elif("Temperature" in mName):
-				Node[nodeidx].temp = mData 
-				#print "Temperature " + str(mData)
-			elif("Ambient" in mName):
-				Node[nodeidx].amblight = mData 
-				#print "Ambient " + str(mData)
-			elif("UV" in mName):
-				Node[nodeidx].UV = mData 
-				#print "UV " + str(mData)
-			elif("RSSI" in mName):
-				Node[nodeidx].RSSI = mData 
-				#print "RSSI " + str(mData)
-			elif("Battery" in mName):
-				Node[nodeidx].Vbat = mData 
-				#print "Vbat " + str(mData)
-		else:#line is empty
-			NumberOfLines = 0
-			#print "Serial read timed out"	
-		if(NumberOfLines ==  MEAS_DATA_LINES):			
-			params = urllib.urlencode({'key': Node[nodeidx].ReadKey, 
-				'field1': Node[nodeidx].temp,
-				'field2': Node[nodeidx].humidity,
-				'field3': Node[nodeidx].amblight,
-				'field4': Node[nodeidx].RSSI,
-				'field5': Node[nodeidx].Vbat})
-			ret = urllib.urlopen("https://api.thingspeak.com/update", 
-				data=params)
-	#		print ret
-	except IndexError:
-		with open("log.txt",'a') as log:
-			log.write(str(datetime.now()) + " Index error. Last data was:\n")
-			log.write(line + "\n")
-	except Exception as e:
-		with open("log.txt",'a') as log:
-			log.write(str(datetime.now()) + " Unhandled exception.\n" + str(e) + "\nLast data was:\n")
-			log.write(line + "\n")
-		
-ser.close()
+adapter = pygatt.GATTToolBackend()
+
+t_uuid = '00002a6e-0000-1000-8000-00805f9b34fb'
+rh_uuid = '00002a6f-0000-1000-8000-00805f9b34fb'
+while True:
+   try:
+      adapter.start()
+      # device = adapter.connect('00:0B:57:07:35:CF')#TBS
+      device = adapter.connect('00:0B:57:51:AF:DE')  # TBS
+      humidity = device.char_read(rh_uuid)
+      temperature = device.char_read(t_uuid)
+      device.disconnect()
+
+      print "Temperature: {}, Humidity: {}"\
+         .format(struct.unpack("<H", temperature)[0]/100.0,
+               struct.unpack("<H", humidity)[0]/100.0)
+      raw_input()
+   finally:
+      adapter.stop()
+
+
+
+#while(True):
+#
+#		if(NumberOfLines ==  MEAS_DATA_LINES):			
+#			params = urllib.urlencode({'key': Node[nodeidx].ReadKey, 
+#				'field1': Node[nodeidx].temp,
+#				'field2': Node[nodeidx].humidity,
+#				'field3': Node[nodeidx].amblight,
+#				'field4': Node[nodeidx].RSSI,
+#				'field5': Node[nodeidx].Vbat})
+#			ret = urllib.urlopen("https://api.thingspeak.com/update", 
+#				data=params)
+#	#		print ret
+#	except IndexError:
+#		with open("log.txt",'a') as log:
+#			log.write(str(datetime.now()) + " Index error. Last data was:\n")
+#			log.write(line + "\n")
+#	except Exception as e:
+#		with open("log.txt",'a') as log:
+#			log.write(str(datetime.now()) + " Unhandled exception.\n" + str(e) + "\nLast data was:\n")
+#			log.write(line + "\n")
+#		
 
